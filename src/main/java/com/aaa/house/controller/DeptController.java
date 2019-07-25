b08b5d2015fe5f07ca4bd48aa374efec6aff0981
qy98jiangxianseng <1262032250@qq.com>
2019-07-25 14:15:18 +0800

    新加了DeptController

---
+++ src/main/java/com/aaa/house/controller/DeptController.java
@@ -0,0 +1,11 @@
+package com.aaa.house.controller;
+
+/**
+ * fileName:DeptController
+ * description:
+ * author:zz
+ * createTime:2019/7/25 14:13
+ * versoin:1.0.0
+ */
+public class DeptController {
+}
